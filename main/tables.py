39362949361c4f3e422c6002d5b3c361412d6303
ruchanguler <gulerruchan@gmail.com>
2021-08-12 19:45:53 +0300

    some bug fixes

--- main/tables.py
+++ main/tables.py
@@ -4,7 +4,7 @@ from .models import Cattle
 class CattleTable(tables.Table):
     class Meta:
         model = Cattle
-        template_name = "django_tables2/bootstrap-responsive.html"
+        template_name = "django_tables2/semantic.html"
         fields = ("buyer","salesorder","cattleid","slaughterorder","phonenumber")
     Sil = tables.TemplateColumn(template_name="links.html",)
     #phonenumber = tables.TemplateColumn(template_name="phonenum.html")
